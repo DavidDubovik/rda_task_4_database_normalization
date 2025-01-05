e5b32e63be982808c95198e7e16f1ad7efee56bd
salodoxera <DavidDubovik19@gmail.com>
2025-01-05 18:12:19 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,33 +1,49 @@
 -- Create database and tables
 
-CREATE DATABASE ShopDB;
+create DATABASE ShopDB;
 USE ShopDB;
 
-CREATE TABLE Countries (
+create TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 );
 
-CREATE TABLE ProductInventory (
+create TABLE Warehouse (
     ID INT,
-    ProductName VARCHAR(50),
     WarehouseAmount INT,
     WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    WarehouseAddress VARCHAR(50),
+	FOREIGN KEY (ID) REFERENCES ProductInventory(ID) ON delete NO ACTION,
     PRIMARY KEY (ID)
 );
 
+create TABLE ProductInventory (
+    ID INT,
+    ProductName VARCHAR(50),
+    WarehouseID  INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouse(ID) ON delete NO ACTION,
+    CountryID INT,
+	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON delete NO ACTION,
+    PRIMARY KEY (ID)
+);
+
+
+
 -- Populate test data
 
-INSERT INTO Countries (ID,Name)
-	VALUES (1, 'Country1');
-INSERT INTO Countries (ID,Name)
-	VALUES (2, 'Country2');
+insert into Countries (ID,Name)
+	values (1, 'Country1');
+insert into Countries (ID,Name)
+	values (2, 'Country2');
     
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+insert into ProductInventory (ID,ProductName, WarehouseID, CountryID)
+	values (1, 'AwersomeProduct',1, 1);
+insert into ProductInventory (ID,ProductName, WarehouseID, CountryID)
+	values (2, 'AwersomeProduct', 2, 2);
+
+insert into Warehouse (ID, WarehouseAmount, WarehouseName, WarehouseAddress)
+	values (1, 2, 'Warehouse-1', 'City-1, Street-1');
+insert into Warehouse (ID, WarehouseAmount, WarehouseName, WarehouseAddress)
+	values (2, 5, 'Warehouse-2', 'City-2, Street-2');
+
